17a17d6d19a1a153dc856cb7302e1aa52253070d
unknown <you@example.com>
2019-06-01 03:06:12 +0200

    BootStrapExamples

---
+++ test.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Meta tags -->
+    <meta charset = "utf-8">
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1, shrink-to-fit = no">
+
+    <title>Alerts</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.1.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+</head>
+<body>
+
+<div class="navbar-collapse collapse">
+    <ul class="nav navbar-right">
+        <!-- start: MESSAGES DROPDOWN -->
+        <li style="padding-top:2% ">
+            <h2>Nile Hospital Management System</h2>
+        </li>
+
+
+        <li class="dropdown current-user">
+            <a href class="dropdown-toggle" data-toggle="dropdown">
+                <img src="images/tea.png"> <span class="username">
+
+                        <i class="ti-angle-down"></i></i></span>
+            </a>
+            <ul class="dropdown-menu dropdown-dark">
+                <li>
+                    <a href="edit-profile.php">
+                        My Profile
+                    </a>
+                </li>
+
+                <li>
+                    <a href="change-password.php">
+                        Change Password
+                    </a>
+                </li>
+                <li>
+                    <a href="logout.php">
+                        Log Out
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <!-- end: USER OPTIONS DROPDOWN -->
+    </ul>
+    <!-- start: MENU TOGGLER FOR MOBILE DEVICES -->
+    <div class="close-handle visible-xs-block menu-toggler" data-toggle="collapse" href=".navbar-collapse">
+        <div class="arrow-left"></div>
+        <div class="arrow-right"></div>
+    </div>
+    <!-- end: MENU TOGGLER FOR MOBILE DEVICES -->
+</div>
+
+<!--</div>-->
+
+<!--<h1>Basic Cardes</h1>-->
+<!--<div class="card">-->
+<!--<div class="card-body">Basic card</div>-->
+<!--</div>-->
+<!--<h1>Contextual Cards</h1>-->
+<!--<div class="card bg-warning text-white">-->
+<!--<div class="card-body">Warning card</div>-->
+<!--</div>-->
+<!--<h1> Card Images</h1>-->
+<!--<div class="card " style="width:400px">-->
+<!--<img class="card-img-top" src="images/pun.png">-->
+<!--<div class="card-body">-->
+<!--<h4 class="card-title">Heba</h4>-->
+<!--<p class="card-text">Some Info about me...</p>-->
+<!--<a href="#" class="btn btn-primary">See Profile</a>-->
+<!--</div>-->
+<!--</div>-->
+<!-- for sizing:  pagination-lg,  pagination-sm -->
+<!-- jQuery first,then Bootstrap JS -->
+<script src="js/jquery-3.1.1.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+
